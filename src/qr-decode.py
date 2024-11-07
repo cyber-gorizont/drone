4252cfdb6297339ef974ae4bd458359f23a4ef54
Alexander <hassiy@disroot.org>
2024-11-07 19:02:28 +0300

    Add new src/qr-decode.py

---
+++ src/qr-decode.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import cv2
+import numpy as np
+import pyzbar.pyzbar as pyzbar
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, frame = cap.read()
+    decodeObjects = pyzbar.decode(frame)
+    for obj in decodeObjects:
+        print("Data", obj.data)
+        cv2.putText(frame, str(obj.data), (50, 50), font, 2, (255, 0, 0), 3)
+    cv2.imshow("Image", frame)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
